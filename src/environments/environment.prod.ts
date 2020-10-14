17ac84b64a3caadaaf7f3bb278fb5bc0332c7efe
Tibing <nik.poltoratsky@gmail.com>
2020-10-14 20:37:19 +0300

    feat: setup firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBjhIXxJxiBfXU1eEmtZ6wgkzOsiEUN8as',
+    authDomain: 'my-todo-app-a6955.firebaseapp.com',
+    databaseURL: 'https://my-todo-app-a6955.firebaseio.com',
+    projectId: 'my-todo-app-a6955',
+    storageBucket: 'my-todo-app-a6955.appspot.com',
+    messagingSenderId: '588505160459',
+    appId: '1:588505160459:web:c624b4a1b4d629580a600e',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBjhIXxJxiBfXU1eEmtZ6wgkzOsiEUN8as',
+    authDomain: 'my-todo-app-a6955.firebaseapp.com',
+    databaseURL: 'https://my-todo-app-a6955.firebaseio.com',
+    projectId: 'my-todo-app-a6955',
+    storageBucket: 'my-todo-app-a6955.appspot.com',
+    messagingSenderId: '588505160459',
+    appId: '1:588505160459:web:c624b4a1b4d629580a600e',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
